fa89fd324fca8e2cb73865f98132064881fee268
lightningsnorlax <ianlimyuyang@outlook.com>
2023-02-25 15:24:27 +0800

    markdown and some html shit
    wdafehj

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,10 +1,13 @@
-() => {
+window.onload = function () {
+    $("#head").hide()
+    $("#body").hide()
+
     var ImageMap = function (map, img) {
         var n,
             areas = map.getElementsByTagName('area'),
             len = areas.length,
             coords = [],
-            previousWidth = 2048;
+            previousWidth = 295;
         for (n = 0; n < len; n++) {
             coords[n] = areas[n].coords.split(',');
         }
@@ -25,10 +28,6 @@
     },
         imageMap = new ImageMap(document.getElementById('body-map'), document.getElementById('map-img'));
     imageMap.resize();
-    imageMap = new ImageMap(document.getElementById('head-map-target'), document.getElementById('head-img'));
-    imageMap.resize();
-    imageMap = new ImageMap(document.getElementById('just-body-map-target'), document.getElementById('just-body-img'));
-    imageMap.resize();
     return;
 }
 
@@ -39,11 +38,64 @@ $(".body-area").click((e) => {
     if (target == "Head") {
         $("#head").show()
         $("#whole-body").hide()
+
+        var ImageMap = function (map, img) {
+            var n,
+                areas = map.getElementsByTagName('area'),
+                len = areas.length,
+                coords = [],
+                previousWidth = 124;
+            for (n = 0; n < len; n++) {
+                coords[n] = areas[n].coords.split(',');
+            }
+            this.resize = function () {
+                var n, m, clen,
+                    x = img.offsetWidth / previousWidth;
+                for (n = 0; n < len; n++) {
+                    clen = coords[n].length;
+                    for (m = 0; m < clen; m++) {
+                        coords[n][m] *= x;
+                    }
+                    areas[n].coords = coords[n].join(',');
+                }
+                previousWidth = document.body.clientWidth;
+                return true;
+            };
+            window.onresize = this.resize;
+        },
+            imageMap = new ImageMap(document.getElementById('head-map-target'), document.getElementById('head-img'));
+        imageMap.resize();
     }
     if (target == "Body") {
         $("#body").show()
         $("#whole-body").hide()
+
+        var ImageMap = function (map, img) {
+            var n,
+                areas = map.getElementsByTagName('area'),
+                len = areas.length,
+                coords = [],
+                previousWidth = 295;
+            for (n = 0; n < len; n++) {
+                coords[n] = areas[n].coords.split(',');
+            }
+            this.resize = function () {
+                var n, m, clen,
+                    x = img.offsetWidth / previousWidth;
+                for (n = 0; n < len; n++) {
+                    clen = coords[n].length;
+                    for (m = 0; m < clen; m++) {
+                        coords[n][m] *= x;
+                    }
+                    areas[n].coords = coords[n].join(',');
+                }
+                previousWidth = document.body.clientWidth;
+                return true;
+            };
+            window.onresize = this.resize;
+        },
+            imageMap = new ImageMap(document.getElementById('just-body-map-target'), document.getElementById('just-body-img'));
+        imageMap.resize();
     }
 
 })
-
