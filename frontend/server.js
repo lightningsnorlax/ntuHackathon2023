3439320532e1a39363564ce6270f96452698a80e
lightningsnorlax <ianlimyuyang@outlook.com>
2023-02-25 16:56:07 +0800

    zdvfdhtfjrdrbzdtnaerrsbrsbzsrzdf

--- frontend/server.js
+++ frontend/server.js
@@ -6,6 +6,35 @@
 
 const express = require("express");
 
+const cheerio = require("cheerio")
+const axios = require("axios")
+
+const url = "https://esmed.org/MRA/mra/search/search"
+
+async function performScraping(query) {
+  // downloading the target web page
+  // by performing an HTTP GET request in Axios
+
+  axios.post(url, {query: "brain"}).then((axiosResponse) => {
+
+    console.log(axiosResponse.data)
+    const $ = cheerio.load(axiosResponse.data)
+    var list = []
+    console.log($(".search-results").text())
+    $(".search-results").find(".media-heading a").each((index, element) => {
+      list.push($(element).attr("href"))
+    })
+    // $(".search-results").each((i,x) => {
+    //   console.log($(x).text())
+    // })
+    console.log(list)
+    // const htmlElement = $('.media-heading a').map((i, x) => $(x).attr('href')).toArray()
+    // console.log(htmlElement)
+  })
+
+}
+
+
 const serveStatic = require("serve-static");
 
 var hostname = "localhost";
@@ -25,15 +54,31 @@ app.use(function (req, res, next) {
 });
 
 app.use(serveStatic(__dirname + "/public"));
+performScraping("brain")
 
 app.get("/", (req, res) => {
-    res.sendFile("/public/html/index.html", { root: __dirname });
+  // console.log(axiosResponse.data)
+  res.sendFile("/public/html/index.html", { root: __dirname });
 });
 
-app.get("/moreInfo", (req, res) => {
-  res.sendFile("/public/html/moreInfo.html", { root: __dirname });
-});
+app.get("/moreinfo", (req, res) => {
+    res.sendFile("/public/html/moreinfo.html", { root: __dirname });
+  });
 
 app.listen(port, () => {
-    console.log("Web App Hosted at http://localhost:%s", port);
+  console.log("Web App Hosted at http://localhost:%s", port);
 });
+
+
+// kaleb shit testing
+// Define route for fetching JSON data
+// app.get('/data', async (req, res) => {
+//   try {
+//     const response = await axios.get('./public/static/medical_spider.json');
+//     const data = response.data;
+//     res.json(data);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Server Error');
+//   }
+// });
